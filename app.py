5477e6c0d97604f6ccc06c630f97d8032c4bef60
Devin <devinchugh.dc@gmail.com>
2021-11-07 19:45:14 +0530

    Created database for tips using SQLAlchemy and sqlite

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from forms import tips
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '6cf7cbc2e952d4ccddc2862d3e646346'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+class tips(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    tip = db.Column(db.Text, nullable=False)
+    timeline = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"tips('{self.id}', '{self.tip}', '{self.timeline}')"
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def submit_tip():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
