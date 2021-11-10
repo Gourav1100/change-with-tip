d9e951384a7387e963c99f952e8be846c249ef44
Devin <devinchugh.dc@gmail.com>
2021-11-10 15:33:14 +0530

    created the table to store admin login details

--- app.py
+++ app.py
@@ -42,6 +42,16 @@ class tips(db.Model):
         return f"tips('{self.id}', '{self.tip}', '{self.timeline}')"
 
 
+class admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable = False)
+
+    def __repr__(self):
+        return f"admin('{self.id}' ,'{self.username}')"
+
+
+
 @app.route("/")
 def hello():
     return "Hello World"
