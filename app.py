1fb682bebb7db33ea689e85380367ab33bcc1cf7
Devin <devinchugh.dc@gmail.com>
2021-11-11 22:55:22 +0530

    Seperated the models and functions from main app

--- app.py
+++ app.py
@@ -1,34 +1,22 @@
 from flask import Flask, request, redirect, render_template
 from flask.helpers import flash
 from flask_sqlalchemy import SQLAlchemy
-#from forms import tips
+from models import tips, Admin, db
+from helpers import encrypt_tip,decrypt_tip,checkdata
 from datetime import datetime, timedelta
-from cryptography.fernet import Fernet
 from werkzeug.security import check_password_hash, generate_password_hash
 from dotenv import load_dotenv   #for python-dotenv method
 load_dotenv()                    #for python-dotenv method
 import os
 from apscheduler.schedulers.background import BackgroundScheduler
 
-# initialising fernet
-key = os.environ.get("FERNET_KEY")
-fernet = Fernet(key)
 
-
+# Initialising the app and environment variables
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '6cf7cbc2e952d4ccddc2862d3e646346'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-db = SQLAlchemy(app)
+db.init_app(app)
 
-def checkdata(data):
-    return True
-
-# Functions to encrypt and decrypt the tips given by user
-def encrypt_tip(data):
-    return fernet.encrypt(data.encode())
-
-def decrypt_tip(data):
-    return fernet.decrypt(data).decode()
 
 # Ensure responses aren't cached
 @app.after_request
@@ -39,63 +27,36 @@ def after_request(response):
     return response
 
 
-class tips(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    tip = db.Column(db.Text, nullable=False)
-    timeline = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __repr__(self) -> str:
-        return f"tips('{self.id}', '{self.tip}', '{self.timeline}')"
-
-
-    # Functions to delete the tips after 24 hours
-    @classmethod
-    def delete_expired(cls):
-        expiration_days = 1
-        limit = datetime.utcnow() - timedelta(expiration_days)
-        cls.query.filter(cls.timeline > limit).delete()
-        db.session.commit()
-
-
-    def delete_expired_tips():
-        tips.delete_expired()
-
 # Scheduling the job to automatically delete the tips after 24 hours
 sched = BackgroundScheduler(daemon=True)
 sched.add_job(tips.delete_expired_tips,'interval',minutes=1)
 sched.start()
 
 
-class Admin(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable = False)
-
-    def __repr__(self):
-        return f"Admin('{self.id}' ,'{self.username}')"
-
-
-
 @app.route("/")
 def hello():
     return "Hello World"
 
+# Route for the user to submit tip to backend
 @app.route("/submit_tip/<data>", methods=["GET","POST"])
 def submit_tip(data):
     if checkdata(data):
         add_tip=tips(tip=encrypt_tip(data))
         db.session.add(add_tip)
         db.session.commit()
-        return redirect("/")
+        return "Tip Submission Successful!"
 
+
+# Route for the Admin to login
 @app.route("/login/<username>/<password>", methods=["POST"])
 def login_admin(username, password):
-    user=admin.query.filter_by(username=username).first()
+    user=Admin.query.filter_by(username=username).first()
     if user and check_password_hash(user.password_hash ,password):
         flash("Login Successful!")
         return redirect("/admin_home")
-    return redirect("/")
+    return True
 
+# Admin Home route to show the tips received
 @app.route("/admin_home")
 def get_tips():
     tips_rec = tips.query.all()
